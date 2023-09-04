ef54ddb988d7cf7b73bed4c5319e518d3406ade0
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-09-05 00:03:18 +0300

    implement buttons

--- usestate/src/Counter.js
+++ usestate/src/Counter.js
@@ -2,8 +2,16 @@ import React from "react"
 
 const Counter = () => {
   return (
-    <div>
+    <div className="container">
       <h2>React Example 1 - Counter</h2>
+      <div className="inner-content">
+        <h1>0</h1>
+        <div className="buttons">
+          <button>Increase</button>
+          <button>Decrease</button>
+          <button>Reset</button>
+        </div>
+      </div>
     </div>
   )
 }
