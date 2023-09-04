83a5f218233c4e83b95cf08a93edd4a1f649dae7
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-09-04 23:09:10 +0300

    initial commit

---
+++ usestate/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Counter from "./Counter"
+
+function App(){
+  return(
+    <React.Fragment>
+      <Counter />
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ usestate/src/Counter.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Counter = () => {
+  return (
+    <div>
+      <h2>React Example 1 - Counter</h2>
+    </div>
+  )
+}
+
+export default Counter
