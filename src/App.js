1089bbc755f1606cfe02bb6560cca5831b2495fa
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-02 21:11:04 +0300

    file restructure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./usestate/Counter";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Counter />
+    </React.Fragment>
+  )
+}
+
+export default App
