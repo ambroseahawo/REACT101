decbc2b640b9c695cbef62cbcbdb5a12d53347b1
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-04 20:35:24 +0300

    Implement handleincrease

--- src/usestate/Counter.js
+++ src/usestate/Counter.js
@@ -4,13 +4,17 @@ import "./style.css"
 const Counter = () => {
   const [number, setNumber] = useState(0)
 
+  const handleIncrease = () => {
+    setNumber(number + 1)
+  }
+
   return (
     <div className="container">
       <h2>React Example 1 - Counter</h2>
       <div className="inner-content">
         <h1>{number}</h1>
         <div className="buttons">
-          <button>Increase</button>
+          <button onClick={handleIncrease}>Increase</button>
           <button>Decrease</button>
           <button>Reset</button>
         </div>
