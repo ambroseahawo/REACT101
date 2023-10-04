736cb8effba37f332999eea804501719b918cf29
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-10-04 20:33:29 +0300

    Initialize state variable

--- src/usestate/Counter.js
+++ src/usestate/Counter.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
+import "./style.css"
 
 const Counter = () => {
+  const [number, setNumber] = useState(0)
+
   return (
     <div className="container">
       <h2>React Example 1 - Counter</h2>
       <div className="inner-content">
-        <h1>0</h1>
+        <h1>{number}</h1>
         <div className="buttons">
           <button>Increase</button>
           <button>Decrease</button>
